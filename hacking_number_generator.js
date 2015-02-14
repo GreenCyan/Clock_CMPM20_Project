94ef5bf12887b9bf4be8b0958b2b3a70e0df99e0
sonhuynh95 <sohuynh95@gmail.com>
2015-02-13 23:05:21 -0800

    Added color functions and parameters

--- hacking_number_generator.js
+++ hacking_number_generator.js
@@ -10,29 +10,14 @@ var DEDUCT_VAR = 0.1;
 var MIN_STATS = 5;
 
 var clickables = new Array();
-var status = new TextBox("Status: ");
 var hackBox = new Sprite();
 var input = new Sprite();
 var string = "";
+
+var hackBool = false;
 /*************************************************************/
 /* this function can be ignored as it is just for testing */
 LoadContent = function() {
-	status.center = false;
-	status.fontSize = 40;
-	status.color = "#660033";
-	
-	status.drawBG = true;
-	status.bgColor = "#FFFFCC";
-	status.border = 10;
-	
-	status.borderColor = "#333300";
-	status.padTop = 10;
-	status.padBottom = 10;
-	status.padLeft = 10;
-	status.padRight = 10;
-	status.alpha = 0.7;
-	world.addChild(status);
-	
 	hackBox.width = 100;
 	hackBox.height = 50;
 	hackBox.x = 20;
@@ -62,7 +47,9 @@ Hack = {
         this.boxType.text = string;
     },
     end: function() {
-        
+        playerStats.end();
+        enemyStats.end();
+	hackBool = false;
     }
 };
 /*************************************************************/
@@ -95,6 +82,13 @@ playerStats = {
         boxMsk.x = 250;
         boxMsk.y = 130;
         world.addChild(boxMsk);
+        
+	playerStats.colorClear();
+	},
+	colorClear: function() {
+		boxAtck.color = "#666633";
+		boxDef.color = "#666633";
+		boxMsk.color = "#666633";
 	},
     statUp: function(theStat) {
     	//input must be a string: the name of the variable
@@ -103,20 +97,27 @@ playerStats = {
     	} else {
     		this[theStat] += MIN_INC;
     	}
-    },
+    	if (box) {
+		if (box.color == "#009900") {
+			box.color = "#CC9900";
+		} else {
+			box.color = "#009900";
+		}
+	}
+	},
     atckUp: function() {
         // generate number here and update boxAtck
-        this.statUp('atck');
+        this.statUp('atck', boxAtck);
         boxAtck.text = this.atck;
     },
     defUp: function() {
         // generate number here and update boxAtck
-        this.statUp('def');
+        this.statUp('def', boxDef);
         boxDef.text = this.def;
     },
     maskUp: function() {
         // generate number here and update boxAtck
-        this.statUp('mask');
+        this.statUp('mask', boxMsk);
         boxMsk.text = this.mask;
     },
     enemyLwrAtck: function() {
@@ -132,7 +133,8 @@ playerStats = {
     	//input must be a string: the name of the variable
     	this[theStat] -= deduct + Math.floor(Math.random() * deduct * DEDUCT_VAR);
     	if (this[theStat] < MIN_STATS) {
-    		this[theStat] = MIN_STATS;
+    		//this[theStat] = MIN_STATS;
+		Hack.end();
     	}
     },
     selfLwrAtck: function(deduct) {
@@ -146,7 +148,12 @@ playerStats = {
     selfLwrMsk: function(deduct) {
         this.statDown('mask', deduct);
         boxDef.text = this.mask;
-    }
+    },
+	end: function() {
+		boxAtck.remove();
+		boxDef.remove();
+		boxMsk.remove();
+	}
 };
 /*************************************************************/
 /* this function stores enemy stats and commands */
@@ -178,6 +185,13 @@ enemyStats = {
 	        box2Sec.x = 350;
 	        box2Sec.y = 130;
 	        world.addChild(box2Sec);
+	        
+	        enemyStats.colorClear();
+	},
+	colorClear: function() {
+		box2Atck.color = "#666633";
+		box2Def.color = "#666633";
+		box2Sec.color = "#666633";
 	},
     statUp: function(theStat) {
     	//input must be a string: the name of the variable
@@ -214,22 +228,35 @@ enemyStats = {
     statDown: function(theStat, deduct) {
     	//input must be a string: the name of the variable
     	this[theStat] -= deduct + Math.floor(Math.random() * deduct * DEDUCT_VAR);
-    	if (this[theStat] < MIN_STATS) {
-    		this[theStat] = MIN_STATS;
+	if (this[theStat] < MIN_STATS) {
+    		//this[theStat] = MIN_STATS;
+		Hack.end();
     	}
+	if (box) {
+		if (box.color == "#009900") {
+			box.color = "#CC9900";
+		} else {
+			box.color = "#FF3300";
+		}
+	}
     },
     selfLwrAtck: function(deduct) {
-        this.statDown('atck', deduct);
+        this.statDown('atck', deduct, box2Atck);
         box2Atck.text = this.atck;
     },
     selfLwrDef: function(deduct) {
-        this.statDown('def', deduct);
+        this.statDown('def', deduct, box2Def);
         box2Def.text = this.def;
     },
     selfLwrSec: function(deduct) {
-        this.statDown('sec', deduct);
+        this.statDown('sec', deduct, box2Sec);
         box2Sec.text = this.sec;
-    }
+    },
+	end: function() {
+		box2Atck.remove();
+		box2Def.remove();
+		box2Sec.remove();
+	}
 };
 function Collision(obj, mouseX, mouseY) {
 	var left = obj.x;
@@ -239,7 +266,6 @@ function Collision(obj, mouseX, mouseY) {
 	if (mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bot) {
 		return true;
 	}
-	status.text = "Click: No";
 	return false;
 }
 /*************************************************************/
@@ -249,55 +275,72 @@ function InputsUpdate() {
 		for (var click in clickables) {
 			click = clickables[click];
 			if (Collision(click, gInput.mouse.x, gInput.mouse.y)) {
-				status.text = "Click: Yes";
                 		Hack.init();
+				hackBool = true;
 			}
 		}
 	};
     input.onKeyDown = function(key) {
         var char = String.fromCharCode(key);
-        switch (key) {
-            case 13:
-                if (string.localeCompare("ATCKUP") == 0 || string.localeCompare("ATTACKUP") == 0) {
-                    playerStats.atckUp();
-                    //condition to increase enemy stat depending on player stat
-                } else if (string.localeCompare("ATCK") == 0 || string.localeCompare("ATTACK") == 0) {
-                    playerStats.enemyLwrDef();
-                    //condition to increase enemy stat depending on player stat
-                }  else if (string.localeCompare("ATCKDOWN") == 0 || string.localeCompare("ATTACKDOWN") == 0 || 
-                	string.localeCompare("ATCKDWN") == 0 || string.localeCompare("ATTACKDWN") == 0 || 
-                	string.localeCompare("ATCKDN") == 0 || string.localeCompare("ATTACKDN") == 0) {
-                    playerStats.enemyLwrAtck();
-                    //condition to increase enemy stat depending on player stat
-                } else if (string.localeCompare("DEFUP") == 0 || string.localeCompare("DEFENSEUP") == 0) {
-                    playerStats.defUp();
-                    //condition to increase enemy stat depending on player stat
-                } else if (string.localeCompare("MASKUP") == 0 || string.localeCompare("MSKUP") == 0 || 
-        		string.localeCompare("MSK") == 0 || string.localeCompare("MASK") == 0) {
-                    playerStats.maskUp();
-                    //condition to increase enemy stat depending on player stat
-                } else if (string.localeCompare("SECDOWN") == 0 || string.localeCompare("SCDOWN") == 0 || 
-        		string.localeCompare("SECDWN") == 0 || string.localeCompare("SCDWN") == 0 || 
-        		string.localeCompare("SECDN") == 0 || string.localeCompare("SCDN") == 0) {
-                    playerStats.enemyLwrSec();
-                    //condition to increase enemy stat depending on player stat
-                }
-                status.text = string;
-                string = "";
-                break;
-            default:
-                if (string.charAt(0) == 0) {
-                    string = string.substring(1);
-                }
-                if (key == 8) {
-                    string = string.substring(0, string.length - 1);
-                } else {
-                    string += char;
-                }
-                break;
-        }
-        Hack.updateTypeBox(string);
-    };
+        if (hackBool == true) {
+	        switch (key) {
+	            case 13:
+	                if (string.localeCompare("ATCKUP") == 0 || string.localeCompare("ATTACKUP") == 0) {
+				playerStats.colorClear();
+				enemyStats.colorClear();
+	                    playerStats.atckUp();
+	                    //condition to increase enemy stat depending on player stat
+	                } else if (string.localeCompare("ATCK") == 0 || string.localeCompare("ATTACK") == 0) {
+				playerStats.colorClear();
+				enemyStats.colorClear();
+	                    playerStats.enemyLwrDef();
+	                    //condition to increase enemy stat depending on player stat
+	                }  else if (string.localeCompare("ATCKDOWN") == 0 || string.localeCompare("ATTACKDOWN") == 0 || 
+	                	string.localeCompare("ATCKDWN") == 0 || string.localeCompare("ATTACKDWN") == 0 || 
+	                	string.localeCompare("ATCKDN") == 0 || string.localeCompare("ATTACKDN") == 0) {
+				playerStats.colorClear();
+				enemyStats.colorClear();
+	                    playerStats.enemyLwrAtck();
+	                    //condition to increase enemy stat depending on player stat
+	                } else if (string.localeCompare("DEFUP") == 0 || string.localeCompare("DEFENSEUP") == 0) {
+				playerStats.colorClear();
+				enemyStats.colorClear();
+	                    playerStats.defUp();
+	                    //condition to increase enemy stat depending on player stat
+	                } else if (string.localeCompare("MASKUP") == 0 || string.localeCompare("MSKUP") == 0 || 
+	        		string.localeCompare("MSK") == 0 || string.localeCompare("MASK") == 0) {
+				playerStats.colorClear();
+				enemyStats.colorClear();
+	                    playerStats.maskUp();
+	                    //condition to increase enemy stat depending on player stat
+	                } else if (string.localeCompare("SECDOWN") == 0 || string.localeCompare("SCDOWN") == 0 || 
+	        		string.localeCompare("SECDWN") == 0 || string.localeCompare("SCDWN") == 0 || 
+	        		string.localeCompare("SECDN") == 0 || string.localeCompare("SCDN") == 0) {
+				playerStats.colorClear();
+				enemyStats.colorClear();
+	                    playerStats.enemyLwrSec();
+	                    //condition to increase enemy stat depending on player stat
+	                } else {
+				playerStats.colorClear();
+				enemyStats.colorClear();
+				//condition to increase enemy stat depending on player stat
+	                }
+	                string = "";
+	                break;
+	            default:
+	                if (string.charAt(0) == 0) {
+	                    string = string.substring(1);
+	                }
+	                if (key == 8) {
+	                    string = string.substring(0, string.length - 1);
+	                } else {
+	                    string += char;
+	                }
+	                break;
+	        }
+	        Hack.updateTypeBox(string);
+        	}
+	};
 	Update();
 }
 Update = function() {
